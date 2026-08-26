965b430bd130bc6e832474893b17baecb32567ee
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-26 21:40:59 +0200

    Apply the four-layer cutover

--- Package.swift
+++ Package.swift
@@ -23,11 +23,11 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            url: "https://github.com/swift-molecules/swift-ascii.git",
             branch: "main"
         ),
         .package(
-            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            url: "https://github.com/swift-molecules/swift-standard-library-extensions.git",
             branch: "main"
         ),
     ],
@@ -35,7 +35,7 @@ let package = Package(
         .target(
             name: "RFC 6750",
             dependencies: [
-                .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
+                .product(name: "ASCII", package: "swift-ascii"),
                 .product(
                     name: "Standard Library Extensions",
                     package: "swift-standard-library-extensions"
